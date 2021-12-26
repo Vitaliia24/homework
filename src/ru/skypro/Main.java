fc28efc75a9eafe4636b33872ce47bdc0e7b01ba
Vitaliia24 <Vitayliyost@mail.ru>
2021-12-26 16:51:43 +0300

    two commit

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,5 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        byte a = 24;
+        short b = 200;
+        int c = 50_000;
+        long d = 89_564_456_321L;
+
+        float e = 4.4563f;
+        double k = 7.656565444458;
+
+        char j = 'X';
+
+        boolean g = false;
+
+
+        float a = 78.2f;
+        float b = 82.7f;
+
+        System.out.println("Общий вес" + (a + b));
+        System.out.println("Разница в весе" + Math.abs(a - b));
+
     }
 }
