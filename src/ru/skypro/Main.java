67de7f336f2680806a5df8c98c238f8ab753c8ac
Vitaliia24 <Vitayliyost@mail.ru>
2021-12-26 19:49:56 +0300

    tree commit

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,8 +3,6 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-
         byte a = 24;
         short b = 200;
         int c = 50_000;
@@ -18,11 +16,81 @@ public class Main {
         boolean g = false;
 
 
-        float a = 78.2f;
-        float b = 82.7f;
+        float z = 78.2f;
+        float q = 82.7f;
 
-        System.out.println("Общий вес" + (a + b));
-        System.out.println("Разница в весе" + Math.abs(a - b));
+        System.out.println("Общий вес" + (z + q));
+        System.out.println("Разница в весе" + Math.abs(z - q));
+
+
+        int bananaWeight = 80;
+        int bananaAmount = 5;
+
+        int milkWeight = 105;
+        int milkAmount = 2;
+
+        int iceWeight = 100;
+        int iceAmount = 2;
+
+        int eggWeight = 70;
+        int eggAmount = 4;
+
+        int result = bananaWeight * bananaAmount +
+                milkWeight * milkAmount + iceWeight * iceAmount
+                + eggWeight * eggAmount;
+
+        System.out.println("Вес коктейля в граммах" + result);
+
+        float resultInKg = result / 1000;
+        System.out.println("Вес коктейля в килограммах" + resultInKg);
+
+
+
+
+        int minusKg = 7;
+
+        int grammInKg = 1000;
+
+        int minusGramm = minusKg * grammInKg;
+
+        int minGrammInDay = 250;
+        int maxGrammInDay = 500;
+
+        int minDays = minusGramm / maxGrammInDay;
+        int maxDays = minusGramm / minGrammInDay;
+
+        int avg = (minDays + maxDays) / 2;
+        System.out.println("Минимальное количество дней" + minDays);
+        System.out.println("Максимальное количество дней" + maxDays);
+        System.out.println("Среднее число дней" + avg);
+
+
+
+
+
+        float mashaZp = 67_760;
+        float denisZp = 83_690;
+        float kristinaZp = 76_230;
+
+        float mashaNewZp = mashaZp + (mashaZp * 0.1);
+        float mashaRaznicaZp = mashaNewZp - mashaZp;
+
+        float denisNewZp = denisZp + (denisZp * 0.1);
+        float denisRaznicaZp = denisNewZp - denisZp;
+
+        float kristinaNewZp = kristinaZp + (kristinaZp * 0.1);
+        float kristinaRaznicaZp = kristinaNewZp - kristinaZp;
+
+        System.out.println("Маша теперь получает" + mashaNewZp + "." +
+                "Годовой доход вырос на" + mashaRaznicaZp + "рублей.");
+        System.out.println("Денис теперь получает" + denisNewZp + "." +
+                "Годовой доход вырос на" + denisRaznicaZp + "рублей.");
+        System.out.println("Кристина теперь получает" + kristinaNewZp + "." +
+                "Годовой доход вырос на" + kristinaRaznicaZp + "рублей.");
+
+
+
+
 
     }
 }
